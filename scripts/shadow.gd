0ed9c283d671bac9edc77acdf057f85808d47faa
xelphyr <xelphyrnull@gmail.com>
2026-01-22 12:00:45 +0530

    fixed a UI bug where the controls for the PhaseToGold and PhaseToBlue transition were blocking inluts to the pause menu, as well as fixed shadows so that they don't generate on phased-out platforms

--- scripts/shadow.gd
+++ scripts/shadow.gd
@@ -2,12 +2,17 @@ extends Decal
 class_name Shadow
 
 @export var shadow_dist : float = 9999
+var collision_mask : int = 0x0001
+
+func _ready() -> void:
+	EventBus.switch_to.connect(func(to:int):
+		collision_mask = pow(2, to-1))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	var parent_pos = get_parent().position
 	var space_state = get_world_3d().direct_space_state
-	var query = PhysicsRayQueryParameters3D.create(parent_pos, parent_pos + Vector3.DOWN * shadow_dist)
+	var query = PhysicsRayQueryParameters3D.create(parent_pos, parent_pos + Vector3.DOWN * shadow_dist, collision_mask, [self])
 	var result = space_state.intersect_ray(query)
 	if result.size() == 0:
 		visible = false
