c3925fb690fdbd12156ac3ad3d3f3c6e3e72a3fa
xelphyr <xelphyrnull@gmail.com>
2026-01-24 14:10:13 +0530

    playing around with jumppad mechanics, fixed xz-plane forces

--- scripts/jump_pad.gd
+++ scripts/jump_pad.gd
@@ -1,10 +1,11 @@
 extends StaticBody3D
 
 @export var jump_force : float = 50
+var xz_mult = 2
 
 func _on_enter_detect_body_entered(body: Node3D) -> void:
 	if body.is_in_group(&"Player"):
 		AudioManager.create_audio(SoundEffectSettings.SoundEffectType.JUMP_PAD)
-		body.velocity += global_transform.basis.y * jump_force
+		body.velocity += global_transform.basis.y * jump_force * Vector3(xz_mult,1,xz_mult)
 
 
